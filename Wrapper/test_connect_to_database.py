317d525e57b27b549e7612155080e510814941a2
samuel <samuel@aims.edu.gh>
2015-07-02 22:04:08 +0000

    added test code to connect python to sqlite database

---
+++ Wrapper/test_connect_to_database.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+conn = sqlite3.connect('/home/samuel/PycharmProjects/CAMS_Workshop/AbcSmc_Python_Wrapper/Database/Wrapper_database.sqlite')
+
+print("Opened Wrapper_database database successfully")
