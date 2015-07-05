5ee1dbe4c1b9bb48a736844035302d217343255e
samuel <samuel@aims.edu.gh>
2015-07-05 17:56:09 +0000

    "updated wrapper"

--- Wrapper/wrapper.py
+++ Wrapper/wrapper.py
@@ -1,52 +1,45 @@
+#!/usr/local/bin/python3
 import sqlite3
 import json
-import sys
-#import 'myscript'
+import argparse
 
 # connect python wrapper to Sqlite
 conn = sqlite3.connect('/home/samuel/PycharmProjects/CAMS_Workshop/AbcSmc_Python_Wrapper/Database/posterior.sqlite')
 print("Opened Wrapper_database database successfully")
 
+parser = argparse.ArgumentParser(description="Run AbcSmc")
+parser.add_argument('myscript', type=str, metavar = 'py_file')
+# parser.add_argument('particles', type=int, default = 1)
+# parser.add_argument('iteration', type=int, default = 1)
+# parser.add_argument('json_file', type=str, default = 'data.json')
 
-# read data from the parameter table in database into a list
+parsed = parser.parse_args()
+
+def simulator() -> None:
+    pass
+
+# open user simulator python script and compile
+exec(open(parsed.myscript).read())
+print("Opened simulator python script successfully")
+
+
+# reading and writing to database
 with conn:
-	mylist = []
-	cur = conn.cursor()  
+	cur = conn.cursor()
 	for row in cur.execute('SELECT * FROM parameters'):
-		mylist.append(list(row))
-	print(mylist)
+		results = simulator(row)
+		cur.execute('insert into metrics values (?,?,?,?,?,?,?,?)', results)
 
-	#write data to metrics table in database
-	for item in mylist:
-		pass
-		#results = myscript.simulator(row) #the rows are to be the input to the simulator function from the python script
-		#cur.execute('insert into metrics values (?,?,?,?,?,?,?,?)', results)
-
-	#test writing unto metrics table
-	a = [1,2,3,4,5,6,7,8]
-	cur.execute('insert into metrics values (?,?,?,?,?,?,?,?)', a)
 
 #function to load .json data into wrapper
 def load_jsondata(jsonfile):
 	if jsonfile[-5:] == '.json':
-		with open(jsonfile) as data_file:     
-		    data = json.load(data_file)
+		with open(jsonfile) as data_file:
+			data = json.load(data_file)
+		print("Opened and loaded json data into wrapper successfully")
 		return data
 	else:
-		return 'Database must have a .json extension'
-
-#def load_simulator(python_simulator):
-#	if python_simulator[-3:] == '.py':
-#		pass
-#		#parse 'python_simulator.py' file into AbcSmc C++ function
+		print('Database must have a .json extension')
 
 
 
-
-jsondata = load_jsondata('data.json') #'data.json' will be replaced with sys.argv[5]
-print(jsondata)
-
-#print examples
-#print(data["maps"][0]["id"])
-#print(data["masks"]["id"])
-#print(data["om_points"])
