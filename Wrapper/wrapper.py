d5ca8464d197dd8910b269c677ed14b47a1a7fb1
samuel <samuel@aims.edu.gh>
2015-07-06 09:00:15 +0000

    "updated wrapper"

--- Wrapper/wrapper.py
+++ Wrapper/wrapper.py
@@ -3,42 +3,51 @@ import sqlite3
 import json
 import argparse
 
-# connect python wrapper to Sqlite
-conn = sqlite3.connect('/home/samuel/PycharmProjects/CAMS_Workshop/AbcSmc_Python_Wrapper/Database/posterior.sqlite')
-print("Opened Wrapper_database database successfully")
-
 parser = argparse.ArgumentParser(description="Run AbcSmc")
-parser.add_argument('myscript', type=str, metavar = 'py_file')
-# parser.add_argument('particles', type=int, default = 1)
-# parser.add_argument('iteration', type=int, default = 1)
-# parser.add_argument('json_file', type=str, default = 'data.json')
+
+
+def pjson(jsonfile):
+	with open(jsonfile) as data_file:
+		try:
+			return json.load(data_file)
+		except ValueError:
+			print("invalid json input")
+			exit()
+
+def loadsim(myscript):
+	exec (open(myscript).read())
+
+
+parser.add_argument('myscript',
+					type=loadsim, metavar='s',
+					help='the .py or .pyc with your def simulate.'
+)
+
+parser.add_argument(
+  'json',
+  default="config.json", metavar="j",
+  help="the .json with the parameters, metrics, and job data",
+  type=pjson
+)
 
 parsed = parser.parse_args()
 
+# connect python wrapper to Sqlite
+conn = sqlite3.connect('/home/samuel/PycharmProjects/CAMS_Workshop/'
+					   'AbcSmc_Python_Wrapper/Database/posterior.sqlite'
+)
+
+
+# parser.add_argument('iteration', type=int, default = 1)
+# parser.add_argument('json_file', type=str, default = 'data.json')
+
+
+
 def simulator() -> None:
-    pass
+  pass  # reading and writing to database
 
-# open user simulator python script and compile
-exec(open(parsed.myscript).read())
-print("Opened simulator python script successfully")
-
-# reading and writing to database
 with conn:
 	cur = conn.cursor()
 	for row in cur.execute('SELECT * FROM parameters'):
 		results = simulator(row)
 		cur.execute('insert into metrics values (?,?,?,?,?,?,?,?)', results)
-
-
-#function to load .json data into wrapper
-def load_jsondata(jsonfile):
-	if jsonfile[-5:] == '.json':
-		with open(jsonfile) as data_file:
-			data = json.load(data_file)
-		print("Opened and loaded json data into wrapper successfully")
-		return data
-	else:
-		print('Database must have a .json extension')
-
-
-#load_jsondata('data.json')
